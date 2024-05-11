96baf6af30a444f71a5664e651d6bbbe3f11fda4
oDustdev <fab.aquiles123@gmail.com>
2024-05-11 17:19:09 -0300

    .

--- src/components/Modals/AuthModal/index.jsx
+++ src/components/Modals/AuthModal/index.jsx
@@ -1,47 +1,70 @@
-import { useAuthModalStore } from '../../../store/modal'
-import { Modal } from '@mui/material';
+import { useAuthModalStore } from "../../../store/modal";
+import { Modal, Box, Fade } from "@mui/material";
 
-export function AuthModal({isOpen, handleClose}) {
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 
+export function AuthModal({ isOpen, handleClose }) {
   // const isOpen = useAuthModalStore((state) => state.isOpen);
   // const setIsOpen = useAuthModalStore((state) => state.setIsOpen);
 
   return (
     <>
-    
-      <Modal open={isOpen}
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={isOpen}
         onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description">
-        <div className="login-panel">
-          <p className="login-title">Insira seu nickname para prosseguir</p>
-          <p className="login-description">
-            O nick não pode conter espaços ou símbolos, insira apenas o nickname
-            que deseja efetuar compras.
-          </p>
-          <form method="post" action="/login">
-            <div className="username">
-              <div className="twisty-net-login">
-                <div id="IGN_MC">
-                  <img
-                    src="https://visage.surgeplay.com/face/32/X-Steve"
-                    alt=""
-                    id="avatar_login"
-                  />
-                  <input
-                    type="text"
-                    name="ign"
-                    id="mc_IGN_INPUT"
-                    autoComplete="off"
-                  />
+        closeAfterTransition
+        slots={{ backdrop: Backdrop }}
+        slotProps={{
+          backdrop: {
+            timeout: 500,
+          },
+        }}
+      >
+        <Fade in={isOpen}>
+          <Box sx={style}>
+            <div className="login-panel">
+              <p className="login-title">Insira seu nickname para prosseguir</p>
+              <p className="login-description">
+                O nick não pode conter espaços ou símbolos, insira apenas o
+                nickname que deseja efetuar compras.
+              </p>
+              <form method="post" action="/login">
+                <div className="username">
+                  <div className="twisty-net-login">
+                    <div id="IGN_MC">
+                      <img
+                        src="https://visage.surgeplay.com/face/32/X-Steve"
+                        alt=""
+                        id="avatar_login"
+                      />
+                      <input
+                        type="text"
+                        name="ign"
+                        id="mc_IGN_INPUT"
+                        autoComplete="off"
+                      />
+                    </div>
+                    <button className="" type="submit">
+                      Prosseguir
+                    </button>
+                  </div>
                 </div>
-                <button className="" type="submit">
-                  Prosseguir
-                </button>
-              </div>
+              </form>
             </div>
-          </form>
-        </div>
+          </Box>
+        </Fade>
       </Modal>
     </>
   );
