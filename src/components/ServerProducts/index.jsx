3a038ab8c178e8f1630c4bf4dd854af2db5bbcd2
oDustdev <fab.aquiles123@gmail.com>
2024-07-11 14:36:51 -0300

    .

--- src/components/ServerProducts/index.jsx
+++ src/components/ServerProducts/index.jsx
@@ -5,6 +5,7 @@ const getProducts = async (filter) => {
   const response = await axios.get(
     `https://backend-twistynetwork.vercel.app/products?filterType=category?filterValue=${filter}`
   );
+  console.log(data);
   return response.data;
 };
 
@@ -19,21 +20,21 @@ export default function ServerProducts({ filter }) {
       {isLoading ? (
         <></>
       ) : (
-        data.map((product) => {
+        data.map((item, index) => {
           <div className="category__list--item">
             <div className="category__list__item--title">
               <img
                 href=""
                 data-remote="/package/5382725"
-                src={product.picture}
+                src={item.picture}
                 className="img-rounded twisty-net__toogle--modal"
               />
             </div>
             <div className="category__list__item--details">
-              <p className="tituloItem">{product.title}</p>
+              <p className="tituloItem">{item.title}</p>
               <div className="__item__price--list">
                 <p className="__item--price">
-                  {product.price} BRL
+                  {item.price} BRL
                   <small />
                 </p>
               </div>
