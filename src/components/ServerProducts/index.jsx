6ecd0bc23c921ee5fdaef6a2bcae7846b9a4c184
oDustdev <fab.aquiles123@gmail.com>
2024-07-22 17:25:16 -0300

    .

--- src/components/ServerProducts/index.jsx
+++ src/components/ServerProducts/index.jsx
@@ -47,6 +47,7 @@ export default function ServerProducts({ filter }) {
               <a
                 onClick={(e) => {
                   e.preventDefault();
+                  alert("ServerProductsModalTest");
                   setProductDetailsId(id);
                   setProductDetailsModalOpen(true);
                 }}
